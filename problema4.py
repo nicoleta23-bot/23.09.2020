7c480463f118ed9b41259607f725bd6ac06caada
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2020-09-23 21:05:11 +0300

    Add files via upload

---
+++ problema4.py
@@ -0,0 +1,14 @@
+print("problema4")
+n = 5
+print(n)
+nmetri = n*100 
+print(nmetri)
+
+nkg = n*(10**6)
+print(nkg)
+
+nluni = n*12
+print(nluni)
+
+nzile = n*365
+print(nzile)
---
+++ problema5.py
@@ -0,0 +1,11 @@
+print("problema5")
+x = True
+y = False
+print("x       y        x and y        y and x        x and x        y and y")
+print(x,"    ",y,"    ",x and y,"    ",y and x,"    ",x and x,"    ",y and y)
+
+print("x       y        x or y        y or x        x or x        y or y")
+print(x,"    ",y,"    ",x or y,"    ",y or x,"    ",x or x,"    ",y or y)
+
+print("x        y        not x        not y")
+print(x,"     ",y,"    ",not x,"    ",not y)
